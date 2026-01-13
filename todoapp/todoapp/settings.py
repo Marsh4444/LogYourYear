afeebe9c4e4592ed7fbcbf535a00dd373b89ded9
Marsh <holyfield444@gmail.com>
2026-01-13 21:18:53 +0100

    Creating the todo app

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     #my apps
+    'todo',
     
 
     #default Django app
--- todoapp/todoapp/views.py
+++ todoapp/todoapp/views.py
@@ -1,11 +1,7 @@
+from todo.models import Task
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
-
-def addTask(request):
-    if request.method == 'POST':
-        task_content = request.POST.get('task')
-        # Here you would typically save the task to the database
-        # For now, we will just redirect back to home
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed=False)
+    context = {'tasks': tasks} 
+    return render(request, 'home.html', context)
