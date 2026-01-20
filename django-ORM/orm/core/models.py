9657e1918573e4dc032396e3a310cb8fc8601abc
Marsh <holyfield444@gmail.com>
2026-01-20 14:49:34 +0100

    Learned about models and migrations

---
+++ django-ORM/orm/core/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+
+
+def validate_restaurant_name_begins_with_a(value):
+    if not value.startswith('a'):
+        raise ValidationError('Restaurant name must begin with "a"')
+
+
+class Restaurant(models.Model):
+    class TypeChoices(models.TextChoices):
+        INDIAN = 'IN', 'Indian'
+        CHINESE = 'CH', 'Chinese'
+        ITALIAN = 'IT', 'Italian'
+        GREEK = 'GR', 'Greek'
+        MEXICAN = 'MX', 'Mexican'
+        FASTFOOD = 'FF', 'Fast Food'
+        OTHER = 'OT', 'Other'
+
+
+    name = models.CharField(max_length=100)
+    website = models.URLField(default='')
+    date_opened = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    resturant_type = models.CharField(max_length=2, choices=TypeChoices.choices)
+
+    def __str__(self):
+        return self.name
+    
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"Rating {self.name}"
+    
+
+class Sale(models.Model):
+    resturant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
+    income = models.DecimalField(max_digits=8, decimal_places=2)
+    datetime = models.DateTimeField()
