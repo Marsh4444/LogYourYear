5b4382ad1619344890c075225299d0b930a4fcdc
Marsh <holyfield444@gmail.com>
2026-02-20 17:18:43 +0100

    search feature done

--- blog1/Blog/views.py
+++ blog1/Blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from bloggss.models import Blog
+from django.db.models import Q
 
 
 def home(request):
@@ -16,5 +17,11 @@ def home(request):
 
 
 def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_desc__icontains=keyword) | Q(content__icontains=keyword)).order_by('-updated_at')
 
-    return render(request, 'search.html')
+    context = {
+        'keyword': keyword,
+        'blogs' : blogs,
+    }
+    return render(request, 'search.html', context)
